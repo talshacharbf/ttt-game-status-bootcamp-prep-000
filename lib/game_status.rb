6b0f380a9b0fe869608c4b60841cee8b9b78342f
tal shachar <tal.shachar@buzzfeed.com>
2017-12-06 00:50:42 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,3 +14,11 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [2,4,6]
 ]
+
+  def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
+  end
+end
